60038ca3886601e18b4994f6fd2ff6418e80f0c2
Nicolás Alancay <nico.alan.alb@gmail.com>
2025-12-10 16:45:28 -0300

    Conversión de requests a formato json

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.status(404).send("Rercurso no encontrado o ruta inválida");
